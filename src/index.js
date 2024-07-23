48618b82f2727d85004b058e7faff7f80953e70c
Raj kumar pandey <rpandeya12345@gmail.com>
2024-07-23 22:26:23 +0530

    third commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log('Hello, world!');
+const express=require('express');
+
+
+const {PORT}=require('./config/serverConfig');
+
+
+const setUpServer=async()=>{
+    const app=express();
+
+    app.listen(PORT,()=>{
+        console.log(`Server is running on Port  ${PORT}`);
+        
+    });
+
+}
+
+
+setUpServer();
